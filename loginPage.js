cb49e3e5422ac15de6f04820357af74c30b3d879
hay <aiyinn0621@gmail.com>
2022-04-23 20:14:49 +0800

    commit

--- loginPage.js
+++ loginPage.js
@@ -3,14 +3,8 @@
 		const password = document.getElementById("logpw");
 		const form = document.getElementById("form");
 
-
-
-	if (svid.value == "" || password.value == "") {
-		alert("Please fill the required fields");
-		return false;
-	}
-
-	else if (password.value.length < 8) {
+while(user.value !== "" && password.value!==""){
+ if (password.value.length < 8) {
 		alert("Your password must include atleast 8 characters");
 		return false;
 	}
@@ -19,6 +13,7 @@
 		return true;
 	}
 }
+	}
 
 function validateSignupForm() {
 	var name = document.getElementById("signName");
@@ -26,42 +21,17 @@ function validateSignupForm() {
 	var unit = document.getElementById("signUnit");
 	var password = document.getElementById("signPassword");
 	var password1 = document.getElementById("signRepeated");
-	var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-	if ( name.value == "" || mail.value == "" || unit.value=="" || password.value == "" || password1.value == "" ) {
-	alert("Please fill the required fields");
-		return false;
-	}
-	else if (password.value.length < 8) {
+while(name.value !== "" && mail.value!=="" && unit.value!=="" && password.value!==""){
+	if (password.value.length < 8) {
 		alert("Your password must include atleast 8 characters");
 		return false;
-	}else if (password.value!= password1.value){
-		alert("Repeated password must be same as password")
-	}else if (!email.value.match(validRegex)) {
-		alert("Please enter correct email ID")
-		document.form.email.focus() ;
+	}else if (password.value!== password1.value){
+		alert("Repeated password must be same as password");
 		return false;
-	}else {
-		alert("Successfully signed up");
-		window.location.href="http://127.0.0.1:5500/southview-fe/sigupPage/signupnoti.html"
-    }
-}
-
-function validateForgetPassword(){
-	var emailID = document.getElementById("email");
-	var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-	if (emailID.value==""){
-		alert("Please fill the required fields");
-	}else if (!emailID.value.match(validRegex)) {
-			alert("Please enter correct email ID")
-			document.form.email.focus() ;
-			return false;
-	} else{
-		alert("Sent successfully.")
-		document.getElementById("resetbtn").addEventListener("click", myFunction);
-		function myFunction(){
-		window.location.href="http://127.0.0.1:5500/southview-fe/sigupPage/signupnoti.html";
 	}
 }
-		}
+
+
+
+		
