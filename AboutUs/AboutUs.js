13a4ee9c62e62b628e0429cb07593808b28597e7
mleong1105 <chanmanleong5@gmail.com>
2022-04-23 13:57:42 +0800

    About Us page

---
+++ AboutUs/AboutUs.js
@@ -0,0 +1,16 @@
+var counter= 1;
+setInterval(function(){
+    document.getElementById('btn' + counter).checked = true;
+    counter++;
+    if (counter > 4){
+        counter = 1;
+    }
+}, 5000);
+
+function initMap(){
+    var location = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Maps(document.getElementById('map'), {
+        zoom: 4,
+        center: location
+    });
+}
