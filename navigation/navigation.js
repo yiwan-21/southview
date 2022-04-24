3c41b397825111a04e20d9e23a3906c389767b62
Lee Yi Wan <leeyiwan0921@gmail.com>
2022-04-25 00:35:44 +0800

    fix navigation js and css

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -10,10 +10,15 @@ function openProfile() {
 fetch('/navigation/navigation.html')
 .then(response => response.text())
 .then(data => {
-    document.body.innerHTML += data;
+    document.body.appendChild(document.createElement('div')).classList.add('nav');
+    const nav = document.querySelector('.nav');
+    nav.style.position = "fixed";
+    nav.style.top = "0";
+    nav.innerHTML += data;
     const template = document.getElementById('navbar-template');
-    document.body.appendChild(template.content);
+    const css = nav.querySelector('link');
+    nav.innerHTML = '';
+    nav.appendChild(template.content);
+    document.head.appendChild(css);
 })
 .catch(err => console.warn('Something went wrong.', err));
-
-
