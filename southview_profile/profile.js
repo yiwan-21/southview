3984432dba2c3d2ac28f602e7898525021500721
PUAJINGYI <puajingyi@gmail.com>
2022-04-29 01:57:21 +0800

    feat: update bug on payment and profile

--- southview_profile/profile.js
+++ southview_profile/profile.js
@@ -1,4 +1,4 @@
-var values=[];
+var values = [];
 function handleFormSubmit(event) {
     event.preventDefault();
     const form = event.target;
@@ -7,6 +7,28 @@ function handleFormSubmit(event) {
         values.push(value)
         console.log(key, value);
     });
-     console.log(values)
-     window.location.href="profile1.html"
+    console.log(values)
+    window.location.href = "profile1.html"
 }
+
+function Submit(event) {
+    event.preventDefault();
+    window.location.href = "/login/login.html"
+}
+
+var check = function () {
+    if (document.getElementById('logpw').value ==
+        document.getElementById('confirmpw').value) {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'Passwords matched';
+    } else {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'Please make sure the passwords are the same';
+    }
+}
+
+function hadSubmit(event) {
+    event.preventDefault();
+    window.location.href = "#resetsuccess"
+}
+
