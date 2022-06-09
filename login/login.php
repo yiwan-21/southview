a08513e3173a75a5aa36a13f4a25e79c53f35f27
hay <aiyinn0621@gmail.com>
2022-06-10 00:58:28 +0800

    feat: login php; session;

--- login/login.php
+++ login/login.php
@@ -1,3 +1,8 @@
+
+<?php
+// include("loginserv.php"); // Include loginserv for checking username and password
+?>
+
 <!DOCTYPE html>
 <html lan="en" and dir="Itr">
     <head>   
@@ -36,13 +41,18 @@
                         
                         <div class="col-lg-4 col-md-4  bg">
                             <h3 class="text-center">Login to Your Account</h3>
-                            <form onsubmit="handleFormSubmit(event)">
+                            <!-- onsubmit="handleFormSubmit(event)" -->
+                            <form action="loginuser.php" method="post" >
                             <table class="table table-borderless"  >
                                 <tr>
+<<<<<<< Updated upstream:login/login.html
                                    <td><input type="text" name="" class="form-control" placeholder="Enter SV ID" required></td> 
+=======
+                                   <td><input type="text" name="svid" class="form-control" placeholder="Enter SV ID" id="svid" required></td> 
+>>>>>>> Stashed changes:login/login.php
                                 </tr>
                                 <tr>
-                                    <td><input type="password" name="" class="form-control" placeholder="Enter Password" id="logpw" inputmode="numeric" minlength="8"
+                                    <td><input type="password" name="password" class="form-control" placeholder="Enter Password" id="logpw" inputmode="numeric" minlength="8"
                                         maxlength="15" size="15" required></td>
                                 </tr>
                                 <tr>
@@ -50,16 +60,29 @@
                                         <div class=" dropdown">   
                                             <h6>Choose your status:</h6>
                                             <label for="status"></label>
+<<<<<<< Updated upstream:login/login.html
                                             <select id="status">
                                             <option value="Administrator">Administrator</option>
                                             <option value="Resident">Resident</option>
+=======
+                                            <select id="status" name='role'>
+                                                <option id="admin" value="Administrator">Administrator</option>
+                                                <option id="resident" value="Resident">Resident</option>
+                                            </select>
+>>>>>>> Stashed changes:login/login.php
                                         </div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
+<<<<<<< Updated upstream:login/login.html
                                         <!-- <a id=loginlink href="/southview-fe/ForgetpwPage/forgetpwnoti.html"> -->
                                         <input type="submit" name="" class="form-control text-center" value="login" onclick="validateLoginForm()" id="loginbtn">
+=======
+                                        <!-- <a id=loginlink href="/southview-fe/Homepage/indexHomepage.html"> -->
+                                        <input type="submit" name="submit" class="form-control text-center" value="LOGIN" id="loginbtn">
+                                        <!-- <button name='submit' id="submitbtn" type="submit" class="form-control text-center">Login</button> -->
+>>>>>>> Stashed changes:login/login.php
                                         <!-- </a> -->
                                     </td>
                                 </tr>
@@ -81,7 +104,16 @@
             <script>
                 function handleFormSubmit(event) {
                     event.preventDefault();
+<<<<<<< Updated upstream:login/login.html
                     window.location.href="/southview-fe/ForgetpwPage/forgetpw.html" //linkxevenpage  
+=======
+                    const status = document.getElementById("status").value;
+                    if (status === "Administrator") {
+                        window.location.href = "/admin/home.html";
+                    } else {
+                        window.location.href = "/Homepage/indexHomepage.html";
+                    }
+>>>>>>> Stashed changes:login/login.php
                 }
             </script>
         </body>
