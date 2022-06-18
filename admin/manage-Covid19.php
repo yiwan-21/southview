0b9bf528bbb68e2c16c14b3bed6ebffa785f08e2
mleong1105 <83906958+mleong1105@users.noreply.github.com>
2022-06-18 22:49:17 +0800

    feat import

--- admin/manage-Covid19.php
+++ admin/manage-Covid19.php
@@ -19,6 +19,16 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
     <!-- custom css -->
     <link rel="stylesheet" href="index.css">
+    
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    
 </head>
 <body>
     <div class="container-fluid">
