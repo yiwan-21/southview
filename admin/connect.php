5ef10c474c0c723e2911a1f7c750e54eb83b690b
LiewPeiMun <peimun030601@gmail.com>
2022-06-18 22:23:34 +0800

    gg

--- admin/connect.php
+++ admin/connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "south_view_2";
+$dbname = "south view";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
